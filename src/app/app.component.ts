b80c970f59f3041797e3da6cc5976e9c7b30cb5f
olimpiogilberto <olimpiogilberto@hotmail.com>
2020-08-17 08:35:19 -0300

    incluido o primeiro component

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class AppComponent {
+  title = 'Livro Angular 2 - Título no app.component.ts';
+  foto: string = 'favicon.ico';
+
+ 
+}
